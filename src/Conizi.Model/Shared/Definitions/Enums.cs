54e71a68b07bd184faa97d8791bc28348c846bd8
Andre Rüb <a.rueb@fleetboard-logistics.com>
2019-05-14 15:29:17 +0200

    Update docu, update comments

--- src/Conizi.Model/Shared/Definitions/Enums.cs
+++ src/Conizi.Model/Shared/Definitions/Enums.cs
@@ -4,28 +4,42 @@ using Conizi.Model.Shared.Attributes;
 
 namespace Conizi.Model.Shared.Definitions
 {
-    public enum LoadType
-    {
-        Unknown = 0,
-        Pickup = 1,
-        DropOff = 2
-    }
-
-    [Description("Determines how the current message should be handled, e.g. whether the stops / consignments should be added removed, ...")]
+    /// <summary>
+    /// Determines how the current message should be handled, e.g. whether the stops / consignments should be added removed, ..."
+    /// </summary>
+    [Description(
+        "Determines how the current message should be handled, e.g. whether the stops / consignments should be added removed, ...")]
     [ConiziAdditionalProperties(false)]
     public enum EdiMessageFunctionCode
     {
-        [EnumMember(Value = "add")]
-        Add = 1,
-        [EnumMember(Value = "update")]
-        Update = 2,
-        [EnumMember(Value = "remove")]
-        Remove = 3,
-        [EnumMember(Value = "add_partial")]
-        AddPartial = 11,
-        [EnumMember(Value = "update_partial")]
-        UpdatePartial = 12,
-        [EnumMember(Value = "remove_partial")]
-        RemovePartial = 13
+        /// <summary>
+        /// Add a new tour
+        /// </summary>
+        [EnumMember(Value = "add")] Add = 1,
+
+        /// <summary>
+        /// Update a tour
+        /// </summary>
+        [EnumMember(Value = "update")] Update = 2,
+
+        /// <summary>
+        /// Remove a tour
+        /// </summary>
+        [EnumMember(Value = "remove")] Remove = 3,
+
+        /// <summary>
+        /// Add a part of a tour
+        /// </summary>
+        [EnumMember(Value = "add_partial")] AddPartial = 11,
+
+        /// <summary>
+        /// Update a part of a tour
+        /// </summary>
+        [EnumMember(Value = "update_partial")] UpdatePartial = 12,
+
+        /// <summary>
+        /// Remove a part of a tour
+        /// </summary>
+        [EnumMember(Value = "remove_partial")] RemovePartial = 13
     }
 }
