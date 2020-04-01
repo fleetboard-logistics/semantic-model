68b662f21e0538a6e6c29351ec18f37dc9b7d195
Andre Rueb <a.rueb@fleetboard-logistics.com>
2020-04-01 16:27:20 +0200

    Fix problems with Time Zones and Date Only

---
+++ test/Conizi.Model.UnitTests/Examples/ExamplesDeserializeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conizi.Model.Core.Tools;
+using Conizi.Model.Examples.Transport.Truck.Groupage.Forwarding;
+using Conizi.Model.Transport.Truck.Groupage.Forwarding;
+using DeepEqual.Syntax;
+using Xunit;
+
+namespace Conizi.Model.UnitTests.Examples
+{
+    public class ExamplesDeserializeTests
+    {
+        [Fact]
+        [Trait("Category", TraitCategory.UNIT_TEST)]
+        public void DeserializeConsignmentSimpleExample_AssertSerializationValid()
+        {
+            var m = new ConsignmentSimpleExample().Create();
+
+            var result = Converter.Serialize(m);
+            Assert.False(result.HasValidationErrors);
+
+            var deserialized = Converter.Deserialize<Consignment>(result.Content);
+
+            m.ShouldDeepEqual(deserialized);
+        }
+    }
+}
