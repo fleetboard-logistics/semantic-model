365e04320282a714c3549b5e716fbda8d281cbef
Steffen Höfner <s.hoefner@fleetboard-logistics.com>
2021-04-14 15:13:15 +0200

    Added LoadingEquipoments to Tour/Consignment

--- src/Conizi.Model/Shared/Entities/EdiLoadingEquipment.cs
+++ src/Conizi.Model/Shared/Entities/EdiLoadingEquipment.cs
@@ -57,17 +57,16 @@ namespace Conizi.Model.Shared.Entities
     /// Information about the type of loading equipment and the amount
     /// </summary>
     public List<EdiLoadingEquipment> LoadingEquipment { get; set; }
-
   }
 
   /// <summary>
-  /// Loading equipment
+  /// Loading equipment Basetype
   /// </summary>
-  [DisplayName("Loading equipment")]
+  [DisplayName("Loading equipment base type")]
   [Description("Loading equipment used for transportation")]
   [ConiziAdditionalProperties(false)]
   [ConiziAllowXProperties]
-  public class EdiLoadingEquipment : EdiPatternPropertiesBase
+  public abstract class EdiLoadingEquipmentBase : EdiPatternPropertiesBase
   {
     /// <summary>
     /// Type of loading equipment like eur pallets, euro boxes...
@@ -84,7 +83,74 @@ namespace Conizi.Model.Shared.Entities
     [DisplayName("Custom Loading Equipment Name")]
     [Description("Custom Loading Equipment Name, like specialCoolingBox...")]
     public string CustomLoadingEquipmentName { get; set; }
+  }
 
+  /// <summary>
+  /// Loading equipment
+  /// </summary>
+  [DisplayName("Loading equipment")]
+  [Description("Loading equipment")]
+  [ConiziAdditionalProperties(false)]
+  [ConiziAllowXProperties]
+  public class EdiLoadingEquipments : EdiPatternPropertiesBase
+  {
+    /// <summary>
+    /// Loading aids which need to be exchanged
+    /// </summary>
+    [DisplayName("Loading equipment for exchange")]
+    [Description("Loading aids which need to be exchanged")]
+    public List<EdiLoadingEquipmentData> EquipmentForExchange { get; set; }
+
+    /// <summary>
+    /// Additional loading aids which are not part of the consignment but which have been added to safely transport the goods
+    /// </summary>
+    [DisplayName("Additional loading equipment")]
+    [Description("Additional loading aids which are not part of the consignment but which have been added to safely transport the goods")]
+    public List<EdiLoadingEquipmentData> AdditionalEquipment { get; set; }
+  }
+
+  /// <summary>
+  /// Loading equipment
+  /// </summary>
+  [DisplayName("Loading equipment")]
+  [Description("Loading equipment")]
+  [ConiziAdditionalProperties(false)]
+  [ConiziAllowXProperties]
+  public class EdiLoadingEquipmentData : EdiLoadingEquipmentBase
+  {
+    /// <summary>
+    /// Amount of equipment
+    /// </summary>
+    [DisplayName("Amount")]
+    [Description("Amount of equipment")]
+    public int Amount { get; set; }
+
+    /// <summary>
+    /// Marks an equipment-exchange to require an exchange reason, if exchange is not complete
+    /// </summary>
+    [DisplayName("Exchange Reason required")]
+    [Description("Marks an equipment-exchange to require an exchange reason, if exchange is not complete")]
+    [JsonRequired]
+    public bool? IsExchangeReasonRequired { get; set; }
+
+    /// <summary>
+    /// Maximum exchange amount
+    /// </summary>
+    [DisplayName("Maximum exchange amount")]
+    [Description("Maximum exchange amount")]
+    public int? MaxExchangeAmount { get; set; }
+  }
+
+
+  /// <summary>
+  /// Loading equipment
+  /// </summary>
+  [DisplayName("Loading equipment")]
+  [Description("Loading equipment used for transportation")]
+  [ConiziAdditionalProperties(false)]
+  [ConiziAllowXProperties]
+  public class EdiLoadingEquipment : EdiLoadingEquipmentBase
+  {
     /// <summary>
     /// Add/Load amount of loading equipment
     /// </summary>
--- src/Conizi.Model/Transport/Truck/Groupage/Forwarding/Tour.cs
+++ src/Conizi.Model/Transport/Truck/Groupage/Forwarding/Tour.cs
@@ -13,175 +13,181 @@ using Newtonsoft.Json.Converters;
 
 namespace Conizi.Model.Transport.Truck.Groupage.Forwarding
 {
+  /// <summary>
+  /// A tour describes an amount of stops given an explicit stop order, containing all information about activities to be done,
+  /// e.g. loading and unloading information about <see cref="Consignment"/>s or maintenance stops
+  /// </summary>
+  [ConiziSchema("https://model.conizi.io/v1/transport/truck/groupage/forwarding/tour.json", "tour.json")]
+  [DisplayName("Tour")]
+  [Description("A tour describes an amount of stops given an explicit stop order, containing all information about activities to be done, e.g. loading and unloading information about consignments or maintenance stops")]
+  [ConiziAdditionalProperties(false)]
+  [ConiziAllowXProperties]
+  public class Tour : EdiModel
+  {
     /// <summary>
-    /// A tour describes an amount of stops given an explicit stop order, containing all information about activities to be done,
-    /// e.g. loading and unloading information about <see cref="Consignment"/>s or maintenance stops
+    /// Message function code
     /// </summary>
-    [ConiziSchema("https://model.conizi.io/v1/transport/truck/groupage/forwarding/tour.json", "tour.json")]
-    [DisplayName("Tour")]
-    [Description("A tour describes an amount of stops given an explicit stop order, containing all information about activities to be done, e.g. loading and unloading information about consignments or maintenance stops")]
-    [ConiziAdditionalProperties(false)]
-    [ConiziAllowXProperties]
-    public class Tour : EdiModel
-    {
-        /// <summary>
-        /// Message function code
-        /// </summary>
-        [JsonConverter(typeof(StringEnumConverter))]
-        [JsonProperty("messageFunctionCode", Order = -11)]
-        [DisplayName("Message function code")]
-        public EdiMessageFunctionCode? MessageFunctionCode { get; set; }
+    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonProperty("messageFunctionCode", Order = -11)]
+    [DisplayName("Message function code")]
+    public EdiMessageFunctionCode? MessageFunctionCode { get; set; }
 
-        /// <summary>
-        /// The Tour Id. A unique identifier assigned to this tour by the devlivering company
-        /// </summary>
-        [DisplayName("Tour Id")]
-        [Description("A unique identifier assigned to this tour by the devlivering company")]
-        [JsonProperty("tourId", Order = -10)]
-        [Required]
-        public string TourId { get; set; }
+    /// <summary>
+    /// The Tour Id. A unique identifier assigned to this tour by the devlivering company
+    /// </summary>
+    [DisplayName("Tour Id")]
+    [Description("A unique identifier assigned to this tour by the devlivering company")]
+    [JsonProperty("tourId", Order = -10)]
+    [Required]
+    public string TourId { get; set; }
 
-        /// <summary>
-        /// A unique identifier for dispatching purposes (Communication between dispatcher and driver)
-        /// </summary>
-        [DisplayName("Tour Reference")]
-        [Description("A unique identifier for dispatching purposes (Communication between dispatcher and driver)")]
-        [JsonProperty("tourReference", Order = -9)]
-        public string TourReference { get; set; }
+    /// <summary>
+    /// A unique identifier for dispatching purposes (Communication between dispatcher and driver)
+    /// </summary>
+    [DisplayName("Tour Reference")]
+    [Description("A unique identifier for dispatching purposes (Communication between dispatcher and driver)")]
+    [JsonProperty("tourReference", Order = -9)]
+    public string TourReference { get; set; }
 
-        /// <summary>
-        /// Identifies the workflow to be used for this tour
-        /// </summary>
-        [DisplayName("Workflow Id")]
-        [Description("Identifies the workflow to be used for this tour")]
-        [JsonProperty(Order = -8)]
-        public string WorkflowId { get; set; }
+    /// <summary>
+    /// Identifies the workflow to be used for this tour
+    /// </summary>
+    [DisplayName("Workflow Id")]
+    [Description("Identifies the workflow to be used for this tour")]
+    [JsonProperty(Order = -8)]
+    public string WorkflowId { get; set; }
 
-        /// <summary>
-        /// The Description. A description of the tour, maybe the occasion of the tour
-        /// </summary>
-        [DisplayName("Description")]
-        [Description("A description of the tour, maybe the occasion of the tour")]
-        [JsonProperty("description", Order = -7)]
-        public string Description { get; set; }
-   
-        /// <summary>
-        /// The Route. Information about the route to be driven or its identification
-        /// </summary>
-        [DisplayName("Route")]
-        [Description("Information about the route to be driven or its identification")]
-        [JsonProperty("route", Order = -6)]
-        public string Route { get; set; }
-       
-        /// <summary>
-        /// Driver Notes. Notes which can be attached by the delivering driver
-        /// </summary>
-        [DisplayName("Driver Notes")]
-        [Description("Notes which can be attached by the delivering driver")]
-        [JsonProperty("driverNotes", Order = -5)]
-        public string DriverNotes { get; set; }
+    /// <summary>
+    /// The Description. A description of the tour, maybe the occasion of the tour
+    /// </summary>
+    [DisplayName("Description")]
+    [Description("A description of the tour, maybe the occasion of the tour")]
+    [JsonProperty("description", Order = -7)]
+    public string Description { get; set; }
 
-        /// <summary>
-        /// Carrier Notes. Notes which can be attached by the delivering driver
-        /// </summary>
-        [DisplayName("Carrier Notes")]
-        [Description("Notes which can be attached by the carrier")]
-        [JsonProperty("carrierNotes", Order = -4)]
-        public string CarrierNotes { get; set; }
+    /// <summary>
+    /// The Route. Information about the route to be driven or its identification
+    /// </summary>
+    [DisplayName("Route")]
+    [Description("Information about the route to be driven or its identification")]
+    [JsonProperty("route", Order = -6)]
+    public string Route { get; set; }
 
-        /// <summary>
-        /// Shipping date
-        /// </summary>
-        [DisplayName("Shipping date")] 
-        [Description("Day on which the tour is proccessed")]
-        [JsonProperty("shippingDate", Order = -3)]
-        [ConiziDateOnly]
-        [JsonConverter(typeof(ConiziDateConverter))]
-        [Required]
-        public DateTime ShippingDate { get; set; }
+    /// <summary>
+    /// Driver Notes. Notes which can be attached by the delivering driver
+    /// </summary>
+    [DisplayName("Driver Notes")]
+    [Description("Notes which can be attached by the delivering driver")]
+    [JsonProperty("driverNotes", Order = -5)]
+    public string DriverNotes { get; set; }
 
-        /// <summary>
-        /// Company responsible for the actual transport of the goods from the shipping partner to the recipient
-        /// </summary>
-        //[DisplayName("Carrier")]
-        //[Description("Company responsible for the actual transport of the goods from the shipping partner to the recipient")]
-        [JsonProperty("carrier", Order = -2)]
-        public EdiAddress Carrier { get; set; }
+    /// <summary>
+    /// Carrier Notes. Notes which can be attached by the delivering driver
+    /// </summary>
+    [DisplayName("Carrier Notes")]
+    [Description("Notes which can be attached by the carrier")]
+    [JsonProperty("carrierNotes", Order = -4)]
+    public string CarrierNotes { get; set; }
 
-        /// <summary>
-        /// Person responsible for dispatching the transport
-        /// </summary>
-        //[DisplayName("Dispatcher")]
-        //[Description("Person responsible for dispatching the transport")]
-        [JsonProperty("dispatcher", Order = -1)]
-        public EdiAddress Dispatcher { get; set; }
+    /// <summary>
+    /// Shipping date
+    /// </summary>
+    [DisplayName("Shipping date")]
+    [Description("Day on which the tour is proccessed")]
+    [JsonProperty("shippingDate", Order = -3)]
+    [ConiziDateOnly]
+    [JsonConverter(typeof(ConiziDateConverter))]
+    [Required]
+    public DateTime ShippingDate { get; set; }
 
-        /// <summary>
-        /// Additional loading aids which are not part of the consignment but which have been added to safely transport the goods
-        /// </summary>
-        public EdiAdditionalLoadingEquipment AdditionalLoadingEquipment { get; set; }
+    /// <summary>
+    /// Company responsible for the actual transport of the goods from the shipping partner to the recipient
+    /// </summary>
+    //[DisplayName("Carrier")]
+    //[Description("Company responsible for the actual transport of the goods from the shipping partner to the recipient")]
+    [JsonProperty("carrier", Order = -2)]
+    public EdiAddress Carrier { get; set; }
 
-        /// <summary>
-        /// Load units (containers, swap bodies, ...) used to transport the goods
-        /// </summary>
-        public List<EdiLoadUnit> LoadUnits { get; set; }
+    /// <summary>
+    /// Person responsible for dispatching the transport
+    /// </summary>
+    //[DisplayName("Dispatcher")]
+    //[Description("Person responsible for dispatching the transport")]
+    [JsonProperty("dispatcher", Order = -1)]
+    public EdiAddress Dispatcher { get; set; }
 
-         /// <summary>
-        /// Notification Parties
-        /// </summary>
-        [DisplayName("Notification Parties")] 
-        [Description("Parties should be notified")]
-        public List<EdiPartnerIdentification> NotificationParties { get; set; }
+    /// <summary>
+    /// Additional loading aids which are not part of the consignment but which have been added to safely transport the goods
+    /// </summary>
+    [Obsolete]
+    public EdiAdditionalLoadingEquipment AdditionalLoadingEquipment { get; set; }
 
-        /// <summary>
-        ///  Information about the drivers of the vehicles
-        /// </summary>
-        public List<EdiDriver> Drivers { get; set; }
+    /// <summary>
+    /// Loading equipment
+    /// </summary>
+    public EdiLoadingEquipments LoadingEquipments { get; set; }
 
-        /// <summary>
-        /// Information about the vehicles used in the transport
-        /// </summary>
-        public List<EdiVehicle> Vehicles { get; set; }
+    /// <summary>
+    /// Load units (containers, swap bodies, ...) used to transport the goods
+    /// </summary>
+    public List<EdiLoadUnit> LoadUnits { get; set; }
 
-        /// <summary>
-        /// Consignments. Consignments which are delivered while processing the tour
-        /// </summary>
-        //[DisplayName("Consignments")]
-        //[Description("Consignments which are delivered while processing the tour")]
-        [JsonProperty("consignments", ReferenceLoopHandling = ReferenceLoopHandling.Ignore, IsReference = false,ItemIsReference = true)]
-        public List<Consignment> Consignments { get; set; }
+    /// <summary>
+    /// Notification Parties
+    /// </summary>
+    [DisplayName("Notification Parties")]
+    [Description("Parties should be notified")]
+    public List<EdiPartnerIdentification> NotificationParties { get; set; }
 
-        /// <summary>
-        ///  Dangerous goods to be declared contained in a consignment 
-        /// </summary>
-        public TourDangerousGoods DangerousGoods { get; set; }
+    /// <summary>
+    ///  Information about the drivers of the vehicles
+    /// </summary>
+    public List<EdiDriver> Drivers { get; set; }
 
-        /// <summary>
-        /// Tour start options. Activities the driver is supposed to do before starting driving the tour
-        /// </summary>
-        public TourStartOptions TourStartOptions { get; set;}
+    /// <summary>
+    /// Information about the vehicles used in the transport
+    /// </summary>
+    public List<EdiVehicle> Vehicles { get; set; }
 
-        /// <summary>
-        /// Tour end options. Activities the driver is supposed to do before starting driving the tour
-        /// </summary>
-        public TourEndOptions TourEndOptions { get; set; }
+    /// <summary>
+    /// Consignments. Consignments which are delivered while processing the tour
+    /// </summary>
+    //[DisplayName("Consignments")]
+    //[Description("Consignments which are delivered while processing the tour")]
+    [JsonProperty("consignments", ReferenceLoopHandling = ReferenceLoopHandling.Ignore, IsReference = false, ItemIsReference = true)]
+    public List<Consignment> Consignments { get; set; }
 
-        /// <summary>
-        ///  Stops for delivering the actual goods
-        /// </summary>
-        public List<EdiTourStop> Stops { get; set; }
+    /// <summary>
+    ///  Dangerous goods to be declared contained in a consignment 
+    /// </summary>
+    public TourDangerousGoods DangerousGoods { get; set; }
 
-        /// <summary>
-        /// Information for the driver to assign the tour to himself
-        /// </summary>
-        public List<EdiSelfDisposal> SelfDisposal { get; set; }
+    /// <summary>
+    /// Tour start options. Activities the driver is supposed to do before starting driving the tour
+    /// </summary>
+    public TourStartOptions TourStartOptions { get; set; }
 
-        /// <summary>
-        /// Additional remarks (free form)
-        /// </summary>
-        [DisplayName("Remarks (free form)")]
-        [Description("Additional remarks")]
-        [StringLength(512)]
-        public string Remarks { get; set; }
-    }
+    /// <summary>
+    /// Tour end options. Activities the driver is supposed to do before starting driving the tour
+    /// </summary>
+    public TourEndOptions TourEndOptions { get; set; }
+
+    /// <summary>
+    ///  Stops for delivering the actual goods
+    /// </summary>
+    public List<EdiTourStop> Stops { get; set; }
+
+    /// <summary>
+    /// Information for the driver to assign the tour to himself
+    /// </summary>
+    public List<EdiSelfDisposal> SelfDisposal { get; set; }
+
+    /// <summary>
+    /// Additional remarks (free form)
+    /// </summary>
+    [DisplayName("Remarks (free form)")]
+    [Description("Additional remarks")]
+    [StringLength(512)]
+    public string Remarks { get; set; }
+  }
 }
